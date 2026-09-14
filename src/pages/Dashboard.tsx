991748784b5fdee555fa59d95c16d68a4d9d263a
Italo.Antonio <italo.ant.mg@gmail.com>
2026-09-14 17:06:08 -0300

    fix: remove importacao nao utilizada do dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import {
   Activity,
-  AlertTriangle,
   CarFront,
   ChevronLeft,
   ChevronRight,
