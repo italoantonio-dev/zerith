f1d664aef261b01ae5b8047a4e7f6e3e29c4e060
Italo.Antonio <italo.ant.mg@gmail.com>
2026-09-14 14:20:33 -0300

    feat(backend): add backend/src/main/java/tech/zerith/api/ZerithApplication.java

---
+++ backend/src/main/java/tech/zerith/api/ZerithApplication.java
@@ -0,0 +1,11 @@
+package tech.zerith.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerithApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZerithApplication.class, args);
+    }
+}
